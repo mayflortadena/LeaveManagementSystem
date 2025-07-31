43217208a40f84def9e48eff527ae7bac1da7199
Mayflor <me.mayflor.tadena@gmail.com>
2025-07-31 16:02:16 +0800

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of MVC Mastery",
+                DateOfBirth = DateTime.Now
+            };
+            return View(data);
+        }
+    }
+}
